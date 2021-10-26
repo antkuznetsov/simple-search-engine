fbab397edbed24209014e297220482fe42286dfd
Anton Kuznetsov <antkuznetsov@yandex.ru>
2021-10-26 22:53:24 +0200

    Implemented Stage 1

--- src/search/Main.java
+++ src/search/Main.java
@@ -1,7 +1,24 @@
 package search;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
+    private final Scanner scanner = new Scanner(System.in);
+    private final Map<String, Integer> data = new HashMap<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Main app = new Main();
+        app.run();
+    }
+
+    private void run() {
+        String[] words = scanner.nextLine().split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            data.put(words[i], i + 1);
+        }
+        String request = scanner.nextLine();
+        System.out.println(data.get(request) == null ? "Not found" : data.get(request));
     }
 }
